9d8c93182cf60d0da40c626e3cd6d2c572987f21
Ananya Patankar <ananya09patankar@gmail.com>
2024-10-11 03:49:24 +0000

    task2.sh

--- task2.sh
+++ task2.sh
@@ -1,1 +1,1 @@
-grep -l "sample" dataset1/* | xargs -I {} sh -c 'count=$(grep -o "CSC510" {} | wc -l); if [ "$count" -ge 3 ]; then size=$(stat -c%s {}); echo "$count $size {}"; fi' | sort -k1,1nr -k2,2n | while read count size file; do echo "${file/file_/filtered_}"; done
+grep -l "sample" dataset1/* | xargs -I{} sh -c 'count=$(grep -o "CSC510" "{}" | wc -l); if [ "$count" -ge 3 ]; then size=$(stat -c%s "{}"); echo "$count $size {}"; fi' | sort -k1,1nr -k2,2nr | gawk '{sub("file_", "filtered_", $3); print $0}' 
