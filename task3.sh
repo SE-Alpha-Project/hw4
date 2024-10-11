60c4c2e0ee3308642f19bd08a0bbcde661d99a4b
Ananya Patankar <ananya09patankar@gmail.com>
2024-10-11 00:41:26 +0000

    task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,1 @@
-awk -F',' 'BEGIN {OFS=","} $3 == 2 && $NF ~ /S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' |  awk -F',' 'BEGIN {OFS=","} {if($7 != "") {sum += $7; count++}} END {if(count > 0) print "Average Age:" sum/count}'
+awk -F',' 'BEGIN {OFS=","} $3 == 2 && $NF ~ /S/ {print $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' |  awk -F',' 'BEGIN {OFS=","} {print $0} {if($7 != "") {sum += $7; count++}} END {if(count > 0) print "Average Age:" sum/count}'
